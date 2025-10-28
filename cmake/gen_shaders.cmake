8f09c26d7d14210cefcfa6711ba8f049eb82d00b
DavidGmc18 <davidgames.youtub@gmail.com>
2025-10-28 15:05:52 +0100

    Updated shader gen script

--- cmake/gen_shaders.cmake
+++ cmake/gen_shaders.cmake
@@ -1,14 +1,15 @@
 file(WRITE "${OUTPUT_CPP}" "#include \"../src/shaders/shaders.hpp\"\n")
 
-file(GLOB SHADER_FILES "${INPUT_DIR}/*")
-foreach(SHADER_FILE IN LISTS SHADER_FILES)
+separate_arguments(INPUT_SHADERS_LIST NATIVE_COMMAND "${INPUT_SHADERS}")
+
+foreach(SHADER_FILE IN LISTS INPUT_SHADERS_LIST)
     get_filename_component(FILE_EXT "${SHADER_FILE}" EXT)
     if(NOT EXISTS "${SHADER_FILE}" OR IS_DIRECTORY "${SHADER_FILE}" OR FILE_EXT STREQUAL ".h")
         continue()
     endif()
 
-    get_filename_component(NAME "${SHADER_FILE}" NAME)
-    get_filename_component(EXT "${NAME}" EXT)
+    get_filename_component(NAME "${SHADER_FILE}" NAME_WE)
+    get_filename_component(EXT "${SHADER_FILE}" EXT)
     string(SUBSTRING "${EXT}" 1 -1 EXT_NO_DOT)
 
     file(READ "${SHADER_FILE}" CONTENTS)
@@ -16,5 +17,5 @@ foreach(SHADER_FILE IN LISTS SHADER_FILES)
     string(REPLACE "\"" "\\\"" ESCAPED_CONTENTS "${CONTENTS}")
     string(REPLACE "\n" "\\n\"\n\"" ESCAPED_CONTENTS "${ESCAPED_CONTENTS}")
 
-    file(APPEND "${OUTPUT_CPP}" "const char* shader_${EXT_NO_DOT}_src = \"${ESCAPED_CONTENTS}\";\n\n")
+    file(APPEND "${OUTPUT_CPP}" "const char* ${NAME}_${EXT_NO_DOT}_src = \"${ESCAPED_CONTENTS}\";\n\n")
 endforeach()
