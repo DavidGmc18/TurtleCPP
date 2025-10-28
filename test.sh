ce06aae229c090cb588066bd9e2887b0fc1a2a1f
DavidGmc18 <davidgames.youtub@gmail.com>
2025-10-28 21:37:17 +0100

    Merge Linux and MacOS tests into test.sh with autodetection

--- test.sh
+++ test.sh
@@ -4,7 +4,15 @@ TESTNAME=${1:-test}
 
 cd "test" || exit 1
 
-g++ "$TESTNAME.cpp" -I../include -L../build -lturtle -Wl,-rpath=../build -o "$TESTNAME.out"
+OS_NAME=$(uname)
+if [[ "$OS_NAME" == "Linux" ]]; then
+    g++ "$TESTNAME.cpp" -I../include -L../build -lturtle -Wl,-rpath=../build -o "$TESTNAME.out"
+elif [[ "$OS_NAME" == "Darwin" ]]; then
+    clang++ "$TESTNAME.cpp" -I../include -L../build -lturtle -rpath ../build -o "$TESTNAME.out"
+else
+    echo "Unsupported OS"
+    exit 1
+fi
 
 "./$TESTNAME.out"
 rm -f *.out
